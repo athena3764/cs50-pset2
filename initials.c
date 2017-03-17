5609774bc9aac24f3ccfb62bc18511c81c6ad94c
Joanne Hsu <Joanne Hsu>
2017-03-16 21:38:07 -0700

    first commit

---
+++ initials.c
@@ -0,0 +1,31 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+/*Implement a program that, given a person’s name, prints a person’s initials, per the below.*/
+
+int main(void){
+    
+    string name;
+    
+    name = GetString();
+    
+    /*Print the first letter in the first name.*/
+   
+    printf("%c", toupper(name[0]));
+   
+    for (int i = 0; i<strlen(name);i++){
+        
+        /*Print the first letters in the middle and/or last names.*/
+       
+        if (name[i]== ' '){
+            
+            printf("%c", toupper(name[i+1]));   
+            
+        }
+   }
+        printf("\n");
+        
+    
+}
