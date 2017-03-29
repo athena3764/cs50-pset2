d12b7880107ec8d6fe01532474b7ad99900401ec
athena3764 <jhhsu11@gmail.com>
2017-03-29 14:45:21 -0700

    Update initials.c

--- initials.c
+++ initials.c
@@ -6,26 +6,17 @@
 /*Implement a program that, given a person’s name, prints a person’s initials, per the below.*/
 
 int main(void){
+  string name;
+  name = GetString();
+  
+  //Print the first letter in the first name.
+  printf("%c", toupper(name[0]));
+  for (int i = 0; i<strlen(name);i++){
     
-    string name;
-    
-    name = GetString();
-    
-    /*Print the first letter in the first name.*/
-   
-    printf("%c", toupper(name[0]));
-   
-    for (int i = 0; i<strlen(name);i++){
-        
-        /*Print the first letters in the middle and/or last names.*/
-       
-        if (name[i]== ' '){
-            
-            printf("%c", toupper(name[i+1]));   
-            
-        }
-   }
-        printf("\n");
-        
-    
+    //Print the first letters in the middle and/or last names.
+    if (name[i]== ' '){
+      printf("%c", toupper(name[i+1]));
+    }
+  }
+  printf("\n");
 }
